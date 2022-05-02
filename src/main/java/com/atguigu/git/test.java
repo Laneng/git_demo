fc4961b71f2bb819798c2fd761a8f1155378b3b8
Lxd <Lxd@qq.com>
2022-05-02 19:44:36 +0800

    first commit

---
+++ src/main/java/com/atguigu/git/test.java
@@ -0,0 +1,12 @@
+package com.atguigu.git;
+
+/**
+ * @author 兰晓东
+ * @create 2022-05-02 19:43
+ */
+public class test {
+    public static void main(String[] args) {
+        System.out.println("hello git");
+
+    }
+}
