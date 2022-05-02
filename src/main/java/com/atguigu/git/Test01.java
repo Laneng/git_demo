89026ddc4db16f19f621217074d192520122be1e
Lxd <Lxd@qq.com>
2022-05-02 20:28:54 +0800

    test

---
+++ src/main/java/com/atguigu/git/Test01.java
@@ -0,0 +1,11 @@
+package com.atguigu.git;
+
+/**
+ * @author 兰晓东
+ * @create 2022-05-02 20:28
+ */
+public class Test01 {
+    public static void main(String[] args) {
+        System.out.println(11111);
+    }
+}
