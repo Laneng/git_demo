8e54f320197fcfefe72385015c2c0121c50f65cd
Lxd <Lxd@qq.com>
2022-05-02 19:46:31 +0800

    thread commit

--- src/main/java/com/atguigu/git/test.java
+++ src/main/java/com/atguigu/git/test.java
@@ -8,6 +8,7 @@ public class test {
     public static void main(String[] args) {
         System.out.println("hello git");
         System.out.println("hello git");
+        System.out.println("hello git");
 
     }
 }
